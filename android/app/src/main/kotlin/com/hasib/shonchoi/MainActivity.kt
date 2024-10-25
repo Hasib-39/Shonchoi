38fe2a1055e86d0e5f7495966317416bf3aeb7ab
Hasib Altaf <hasibaltaf@iut-dhaka.edu>
2024-10-25 17:01:31 +0600

    Initialized adding the heading, Main Card on Home Screen

---
+++ android/app/src/main/kotlin/com/hasib/shonchoi/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.hasib.shonchoi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:shonchoi/app.dart';
+
+void main(){
+  runApp(const MyApp());
+}
